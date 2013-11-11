680c899d0c5646fe7ab438af3f21951cc422508c
TomohiroTanimura <g1144777@cse.kyoto-su.ac.jp>
2013-11-11 14:45:11 +0900

    Downloader スタブ

--- primeministers/primeministers/Downloader.java
+++ primeministers/primeministers/Downloader.java
@@ -10,4 +10,19 @@ public class Downloader extends IO
 	 * まず、次のページを参照しながら、スケルトン（スタブ）を作ることから始めましょう。
 	 * http://www.cc.kyoto-su.ac.jp/~atsushi/Programs/CSV2HTML/PrimeMinistersJavaDoc/index.html
 	 */
+	//フィールド//
+	private java.lang.String url;
+	//コンストラクタ//
+	public Downloader(){}
+	//メソッド//
+	public void downloadCSV(){}
+	public void downloadImages(){}
+	private void downloadPictures(int indexOfPicture){}
+	public void downloaadThumbnails(){}
+	public Table table(){
+	//Overrides:table in class IO
+	}
+	public java.lang.String url(){}
+	public static java.lang.String urlString(){}
+	public static java.lang.String urlStringOfCSV(){}
 }
