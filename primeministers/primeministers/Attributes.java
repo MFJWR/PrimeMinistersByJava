6e08d0eba2f4bc0543d1afa93f4c7882ee3a72b6
TomohiroTanimura <g1144777@cse.kyoto-su.ac.jp>
2013-11-11 14:34:39 +0900

    Attributes 更新

--- primeministers/primeministers/Attributes.java
+++ primeministers/primeministers/Attributes.java
@@ -5,15 +5,38 @@ package primeministers;
  */
 public class Attributes extends Object
 {
-	
-	
-	
-	
-	
-	
 	/**
 	 * ここを作成してください。
 	 * まず、次のページを参照しながら、スケルトン（スタブ）を作ることから始めましょう。
 	 * http://www.cc.kyoto-su.ac.jp/~atsushi/Programs/CSV2HTML/PrimeMinistersJavaDoc/index.html
 	 */
+	//フィールド//
+	private java.util.ArrayList<java.lang.String> keys;
+	private java.util.ArrayList<java.lang.String> names;
+	
+	//コンストラクタ//
+	public Attributes(java.lang.String aString){}
+	
+	//メソッド//
+	protected java.lang.String at(int index){}
+	private int indexOf(java.lang.String aString){}
+	public int indexOfDays(){}
+	public int indexOfImage(){}
+	public int indexOfKana(){}
+	public int indexOfName(){}
+	public int indexOfNo(){}
+	public int indexOfOrder(){}
+	public int indexOfParty(){}
+	public int indexOfPeriod(){}
+	public int indexOfPlace(){}
+	public int indexOfSchool(){}
+	public int indexOfThumbnail(){}
+	public java.util.ArrayList<java.lang.String> keys(){}
+	protected java.lang.String nameAt(int index){}
+	public java.util.ArrayList<java.lang.String> names(){}
+	public void names(java.util.ArrayList<java.lang.String> aCollection){}
+	public int size(){}
+	public java.lang.String toString(){
+	//Overrides:toString in class java.lang.Object
+	}
 }
