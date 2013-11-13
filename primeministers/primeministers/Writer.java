408a40ae7972e8fa32f03c496fd2bb8f07da5ef2
TomohiroTanimura <g1144777@cse.kyoto-su.ac.jp>
2013-11-13 10:21:59 +0900

    Writer スタブ コメント

--- primeministers/primeministers/Writer.java
+++ primeministers/primeministers/Writer.java
@@ -10,4 +10,56 @@ public class Writer extends IO
 	 * まず、次のページを参照しながら、スケルトン（スタブ）を作ることから始めましょう。
 	 * http://www.cc.kyoto-su.ac.jp/~atsushi/Programs/CSV2HTML/PrimeMinistersJavaDoc/index.html
 	 */
+	
+	/**
+	 * ライタのコンストラクタ
+	 */
+	public Writer(){}
+	
+	
+	
+	/**
+	 * ライタのメソッド
+	 */
+	
+	/**
+	 * 属性リストを応答する
+	 */
+	public Attributes attributes(){}
+	
+	/**
+	 * ローカルなHTMLのインデックスファイル(index.html)を応答するクラスメソッド
+	 */
+	public static java.io.File filenameOfHTML(){}
+	
+	/**
+	 * HTMLページを基にするテーブルを受け取って、インデックスファイル(index.html)に書き出す
+	 */
+	public Table table(Table aTable){}
+	
+	/**
+	 * タプル群を応答する
+	 */
+	public java.util.ArrayList<Tuple> tuples(){}
+	
+	/**
+	 * 属性リストを書き出す
+	 */
+	public void writeAttributesOn(java.io.BufferedWriter outputWriter){}
+	
+	/**
+	 * フッタを書き出す
+	 */
+	public void writeFooterOn(java.io.BufferedWriter outputWriter){}
+
+	/**
+	 * ヘッダを書き出す
+	 */
+	public void writeHeaderOn(java.io.BufferedWriter outputWriter){}
+	
+	/**
+	 * ボディを書き出す
+	 */
+	public void writeTableBodyOn(java.io.BufferedWriter outputWriter){}
 }
+
